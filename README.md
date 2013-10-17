15b1ad8f4fd8153f726c9eaa299f2813c4607585
Clayton Daley <clayton.daley@gmail.com>
2013-10-16 20:19:33 -0400

    Adds the "list" action to the API.  This action takes a long URL and returns (1) an array of keywords associated with that long URL and (2) the base URL for yourls so full shorturls can be constructed.

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
-yourls-api-list
-===============
+YOURLS Plugin:  API Action - List Keywords for URL
+==================================================
 
-Plugin for YOURLS adding a "list" action to the API returning all keywords that forward to a specific address
+Plugin for YOURLS adding a "list" action to the API that returns all keywords that forward to a specific URL
+
+# What for
+
+This plug-in adds the "list" action to the YOURLS API.
+`http://my.url/yourls-api.php?username=x&password=xx&action=list&url=http://my.domain.com/my/long/url&format=json` 
+
+A successful API call returns an array(
+	'statusCode' => 200,
+	'simple'     => 'Need either XML or JSON format for list',
+	'message'    => 'success',
+	'list'       => array
+		( 
+		'baseurl'    => [yourls base url with trailing slash],
+		'keywords'   => array
+			(
+			0 => [keyword1],
+			1 => [keyword2],
+			),
+		)
+	)
+
+# How to
+
+* In `/user/plugins`, create a new folder named `yourls-api-list`
+* Drop these files in that directory
+* Go to the Plugins administration page and activate the plugin 
+* Have fun
---
+++ plugin.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Plugin Name: API Action - List Keywords
+Plugin URI: http://www.claytondaley.com/
+Description: Adds a "list" action to the API. This action accepts a long URL through the "url" parameter and returns two values (1) an array of keywords and (2) the base URL required to construct shorturls for these keywords.
+Version: 1.0
+Author: Clayton Daley (derived from API-Action by Ozh)
+Author URI: http://www.claytondaley.com/
+*/
+
+// Define custom action "delete"
+yourls_add_filter( 'api_action_delete', 'clayton_api_action_list' );
+
+/**
+ * API function wrapper: List all shorturls that redirect to a URL
+ *
+ * @since prototype
+ * @return array Result of API call
+ */
+
+function clayton_api_action_list() {
+	// Need 'url' parameter
+	if( !isset( $_REQUEST['url'] ) ) {
+		return array(
+			'statusCode' => 400,
+			'simple'     => "Need a 'url' parameter",
+			'message'    => 'error: missing param',
+		);	
+	}
+	
+	$url = $_REQUEST['url'];
+
+	// Check if valid url
+	if( filter_var($url, FILTER_VALIDATE_URL) === FALSE ) {
+		return array(
+			'statusCode' => 404,
+			'simple '    => 'Error: URL not found or not valid',
+			'message'    => 'error: not found or not valid',
+		);	
+	}
+	
+	// Prevent DB flood
+	$ip = yourls_get_IP();
+	yourls_check_IP_flood( $ip );
+	
+	// Get and return keyword list
+	$keywords = yourls_get_keywords($url);
+	$return = array(
+		'statusCode' => 200,
+		'simple'     => 'Need either XML or JSON format for list',
+		'message'    => 'success',
+		'list'       => array( 
+			'baseurl'    => YOURLS_SITE . '/',
+		    'keywords'   => $keywords,
+            )
+		);
+	return yourls_apply_filter( 'api_result_list', $return );
+	
+}
